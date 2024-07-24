2da5544b38882bb483ca6ef0002cdf7f49c69776
clau <clau.quevv@gmail.com>
2024-07-24 09:23:31 -0500

    actu

--- Proyecto_Final/src/App.java
+++ Proyecto_Final/src/App.java
@@ -1,6 +1,6 @@
+import Controlador.LaberintoControlador;
 import Modelo.Laberinto;
 import Vista.LaberintoGui;
-import Controlador.LaberintoControlador;
 
 public class App {
     public static void main(String[] args) {
@@ -9,5 +9,19 @@ public class App {
         LaberintoControlador controlador = new LaberintoControlador(modelo, vista);
 
         vista.setVisible(true);
+        // boolean[][] grid = {
+        //     {true, true, false, true},S
+        //     {true, false, true, true},
+        //     {true, true, true, false},
+        //     {false, true, true, true}
+        // };
+        // int inicioX = 0, inicioY = 0;
+        // int finX = 3, finY = 3;
+
+        // LaberintoControlador controlador = new LaberintoControlador(grid, inicioX, inicioY, finX, finY);
+        // controlador.ejecutarMetodos();
+
+
+
     }
 }
